e1dea0a6df9a563df8d4759de19d6b2aa6e2dfef
unknown <>
2016-04-29 22:10:45 +0900

    fitst commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+http.createServer(function(req, res){
+	//서버작업
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.end('<h1>Hello World</h1>');
+
+}).listen('8888', function(){ //8888포트에 서버 열겠다
+	console.log('create server!!!');
+});
